a30e6960a69c2eeb5a618055cba1fd2efac3ef7e
angel-721 <angelvelasquez2002@gmail.com>
2025-07-13 17:03:53 -0600

    Add images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,13 @@
+import cat from "../assets/cat.webp";
+import lyric from "../assets/lyric.webp";
+import mva from "../assets/mva.webp";
+import type { ImageMetadata } from "astro";
+
 export interface Project {
   title: string;
   description: string;
   link: string;
+  image: ImageMetadata;
 }
 
 export const projects: Project[] = [
@@ -10,12 +16,14 @@ export const projects: Project[] = [
     description:
       "Research project that utilizes principal component analysis to explore Southern Utah water trends and measure feature importance.",
     link: "https://repository.utahtech.edu/items/da074a0e-cd24-4b12-9b0e-faf8d2aa0ce3",
+    image: mva,
   },
   {
     title: "Lyric-Luminary",
     description:
       "This is a fun project that aims to view lyrics via the lens of a text classification model. I'm experimenting with TF-IDF to classify songs into a genere via the lyrics of the song.",
     link: "https://github.com/angel-721/lyric-luminary",
+    image: lyric,
   },
   {
     title: "Godot Image Classification",
@@ -23,5 +31,6 @@ export const projects: Project[] = [
       "Use a pre-trained convolutional neural network to make in game predictions! Take screenshots of the game and classify if a cat or dog is in the scene with the power of machine learning.",
 
     link: "https://github.com/angel-721/godot-image-classification",
+    image: cat,
   },
 ];
